61008d28820b5da6252d597ee8087b290c7de96d
shaik aqsa fathima <shaikaqsa.78@gmail.com>
2025-11-25 11:22:46 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,11 @@
+ class evenodd {
+   public static boolean num(int n){
+        if(n%2==0) {
+            return true;
+    }
+    return false;
+}
+public static void main(String args[]){
+    System.out.println(num(4));
+}
+ }
