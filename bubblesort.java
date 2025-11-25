1e2bd058b2d42c95237e9c9a7891ba47356c98c7
shaik aqsa fathima <shaikaqsa.78@gmail.com>
2025-11-25 11:23:16 +0530

    Add files via upload

---
+++ bubblesort.java
@@ -0,0 +1,23 @@
+class bubblesort {
+    public static void bubble(int[] arr){
+        int n = arr.length;
+
+        for(int i = 0; i < n-1; i++){
+            for(int j = 0; j < n-1-i; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        int[] arr = {3,6,7,9,5,8,0,1,2,4};
+        bubble(arr);
+        for(int x : arr)
+            System.out.print(x+" ");
+    }
+}
+
